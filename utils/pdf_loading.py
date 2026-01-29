6f7d31a9e74b324dbe483a32a2e23aebaf3fab05
elhidarinouhayla <elhidarinouhayla@gmail.com>
2026-01-29 16:44:30 +0100

    create loading function

--- utils/pdf_loading.py
+++ utils/pdf_loading.py
@@ -1,16 +1,8 @@
-from pypdf import PdfReader
-
 from langchain_community.document_loaders import PyPDFLoader
 
 
 
-pdf_path = "C:/Users/hp/Desktop/IT-Support-RAG-Assistant/data/raw/data.pdf"
-
-loader = PyPDFLoader(pdf_path)
-
-pages = loader.load()
-
-print(f" PDF charge avec succes")
-print(f"   Nombre de pages: {len(pages)}")
-print(pages)
-
+def load_pdf(pdf_path: str):
+    loader = PyPDFLoader(pdf_path)
+    pages = loader.load()
+    return pages
