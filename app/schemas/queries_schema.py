17455682cce89890f071a10eacf48a157123f79d
elhidarinouhayla <elhidarinouhayla@gmail.com>
2026-02-03 15:12:36 +0100

    mise a jour du basemodel et schema du queries

--- app/schemas/queries_schema.py
+++ app/schemas/queries_schema.py
@@ -2,11 +2,13 @@ from pydantic import BaseModel
 
 
 class QueryCreate(BaseModel):
-    userid : int
-    question : str
-    answer : str
-    cluster : int
+    user_id: int
+    question: str
+    answer: str
+    cluster: int
+    latency_ms: float
+    
 
 
-class UserResponse(QueryCreate):
+class QueryResponse(QueryCreate):
     id : int
